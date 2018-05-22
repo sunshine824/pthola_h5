8216ca810f7102fab11c70d5339a8ee81dffd1a1
sunshine824 <790168710@qq.com>
2018-05-22 11:51:54 +0800

    right calendar

--- src/components/bookList.vue
+++ src/components/bookList.vue
@@ -2,57 +2,104 @@
   <div class="book clearfix">
     <div class="top-list">
       <div class="date">
-        <span class="year">2018</span>
-        <span class="month">5月</span>
+        <span class="year">{{date.year}}</span>
+        <span class="month">{{date.month}}月</span>
       </div>
       <div class="menu">
-        <div class="tip">
-          <span class="day today">21</span>
-          <span class="week">周一</span>
-        </div>
-        <div class="tip">
-          <span class="day">21</span>
-          <span class="week">周一</span>
-        </div>
-        <div class="tip">
-          <span class="day">21</span>
-          <span class="week">周一</span>
-        </div>
-        <div class="tip">
-          <span class="day">21</span>
-          <span class="week">周一</span>
-        </div>
-        <div class="tip">
-          <span class="day">21</span>
-          <span class="week">周一</span>
-        </div>
-        <div class="tip">
-          <span class="day">21</span>
-          <span class="week">周一</span>
-        </div>
-        <div class="tip">
-          <span class="day">21</span>
-          <span class="week">周一</span>
+        <div class="tip" v-for="(item,index) in calendarDate">
+          <span class="day" :class="{today:item.isToDay}">{{item.day}}</span>
+          <span class="week">周{{item.week}}</span>
         </div>
       </div>
     </div>
     <div class="lists" :style="{height: 1.281 * time + 'rem'}">
+      <!--左侧时间段-->
       <div class="time">
         <div class="tip" v-for="(item,index) in time">
-          <span class="num">{{String(index).length > 1 ? String(index) : 0+String(index)}}</span>
-          <span class="txt">:00</span>
+          <p class="tim">
+            <span class="num">{{String(index).length > 1 ? String(index) : 0+String(index)}}</span>
+            <span class="txt">:00</span>
+          </p>
         </div>
+        <p class="last-time">
+          <span class="num">24</span>
+          <span class="txt">:00</span>
+        </p>
+      </div>
+      <!--右侧排课列表-->
+      <div class="calendar">
+        <p class="ver-line"
+           v-for="(item,index) in verLine"
+           :style="{left:1.281 * index + 'rem'}">
+        </p>
+        <p class="cross-line"
+           v-for="(item,index) in crossLine"
+           :style="{top:1.281 * index + 'rem'}">
+        </p>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+  import moment from 'moment'
+
   export default {
     name: "book-list",
     data() {
       return {
-        time: 24
+        time: 24,
+        verLine: 7,
+        crossLine: 24,
+        date: {
+          year: moment().format('YYYY'),
+          month: moment().format('MM')
+        },
+        calendarDate: []
+      }
+    },
+    created() {
+      this.getCalendarDate()
+    },
+    methods: {
+      getCalendarDate() {
+        const that = this
+        const now = new Date()
+        this.calendarDate = []
+        for (let i = 0; i < 7; i++) {
+          this.calendarDate.push({
+            day: moment().add(i, 'days').format('DD'),
+            week: that.chinaWeek(moment().add(i, 'days').format('d')),
+            isToDay:now.getDay() == moment().add(i, 'days').format('d')
+          })
+        }
+      },
+      chinaWeek(num) {
+        let week = ''
+        switch (Number(num)) {
+          case 0:
+            week = '日';
+            break;
+          case 1:
+            week = '一';
+            break;
+          case 2:
+            week = '二';
+            break;
+          case 3:
+            week = '三';
+            break;
+          case 4:
+            week = '四';
+            break;
+          case 5:
+            week = '五';
+            break;
+          case 6:
+            week = '六';
+            break
+        }
+        return week
       }
     }
   }
@@ -127,21 +174,69 @@
       display: -webkit-flex;
       flex-flow: row nowrap;
       top: 1.6rem;
-      .time{
+      .time {
         width: 1rem;
         height: 100%;
-        border-right: 1px solid #d9d9d9;
-        .tip{
+        display: flex;
+        display: -webkit-flex;
+        flex-flow: column nowrap;
+        position: relative;
+        .tip {
           width: 100%;
           height: 1.281rem;
           display: flex;
           display: -webkit-flex;
-          align-items: baseline;
-          justify-content: center;
-          flex-flow: row nowrap;
-          .txt{
+          align-items: center;
+          justify-content: space-between;
+          flex-flow: column nowrap;
+          .tim {
+            display: flex;
+            display: -webkit-flex;
+            flex-flow: row nowrap;
+            align-items: baseline;
+            justify-content: center;
+          }
+          .txt {
             font-size: .3rem;
           }
+          &:nth-child(2) {
+            margin-top: -.16rem;
+          }
+        }
+        .last-time {
+          bottom: 0;
+          display: flex;
+          display: -webkit-flex;
+          flex-flow: row nowrap;
+          align-items: baseline;
+          justify-content: center;
+          position: absolute;
+          left: .12rem;
+          .txt {
+            font-size: .3rem;
+          }
+        }
+      }
+      .calendar {
+        position: relative;
+        left: 0;
+        display: flex;
+        flex: 1;
+        background-color: #f7f7f7;
+        z-index: 8;
+        .ver-line {
+          position: absolute;
+          top: 0;
+          border-left: 1px solid #d9d9d9;
+          z-index: 10;
+          height: 100%;
+        }
+        .cross-line {
+          position: absolute;
+          left: 0;
+          width: 100%;
+          border-top: 1px solid #d9d9d9;
+          z-index: 10;
         }
       }
     }
